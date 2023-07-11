ad98c9f14cbd220bed23fb5ca9dee7f56d6cbf7f
hothanhnhank1c <37531502+hothanhnhank1c@users.noreply.github.com>
2023-07-11 22:23:46 +0700

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=admin
-export POSTGRES_HOST=localhost
+export POSTGRES_PASSWORD=nhanht18
+export POSTGRES_HOST=cdr-1.ct6hw0wplhhc.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::nhanht18
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
