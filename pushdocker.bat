5c8f06bebb58a39af2d90a780b28d38fec35f393
hothanhnhank1c <37531502+hothanhnhank1c@users.noreply.github.com>
2023-07-12 21:38:33 +0700

    up

--- pushdocker.bat
+++ pushdocker.bat
@@ -1,12 +1,12 @@
-docker tag udagram-api-feed hothanhnhank1c/udagram-api-feed:v8
-docker tag udagram-api-user hothanhnhank1c/udagram-api-user:v8
-docker tag reverseproxy hothanhnhank1c/reverseproxy:v8
-docker tag udagram-frontend hothanhnhank1c/udagram-frontend:v8
+docker tag udagram-api-feed hothanhnhank1c/udagram-api-feed:v4
+docker tag udagram-api-user hothanhnhank1c/udagram-api-user:v4
+docker tag reverseproxy hothanhnhank1c/reverseproxy:v4
+docker tag udagram-frontend hothanhnhank1c/udagram-frontend:v4
 
-docker push hothanhnhank1c/udagram-api-feed:v8
-docker push hothanhnhank1c/udagram-api-user:v8
-docker push hothanhnhank1c/reverseproxy:v8
-docker push hothanhnhank1c/udagram-frontend:v8
+docker push hothanhnhank1c/udagram-api-feed:v4
+docker push hothanhnhank1c/udagram-api-user:v4
+docker push hothanhnhank1c/reverseproxy:v4
+docker push hothanhnhank1c/udagram-frontend:v4
 
 kubectl autoscale deployment backend-feed --cpu-percent=50 --min=1 --max=2
 kubectl autoscale deployment backend-reverseproxy --cpu-percent=80 --min=1 --max=2
